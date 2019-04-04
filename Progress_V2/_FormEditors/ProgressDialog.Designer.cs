b1891c6e95a04753a9c87a52ebe8d81783b5e0c1
Zeroit Dev <zeroitdevnet@gmail.com>
2019-04-04 13:31:23 +0000

    Added Copyright Information to the Files

--- Progress_V2/_FormEditors/ProgressDialog.Designer.cs
+++ Progress_V2/_FormEditors/ProgressDialog.Designer.cs
@@ -1,4 +1,34 @@
-﻿namespace Zeroit.Framework.ProgressThematic.FormEditors
+﻿// ***********************************************************************
+// Assembly         : Zeroit.Framework.ProgressThematic
+// Author           : ZEROIT
+// Created          : 11-22-2018
+//
+// Last Modified By : ZEROIT
+// Last Modified On : 12-12-2018
+// ***********************************************************************
+// <copyright file="ProgressDialog.Designer.cs" company="Zeroit Dev Technologies">
+//    This program is for creating a Progress control with an Editor.
+//    Copyright ©  2017  Zeroit Dev Technologies
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//    You can contact me at zeroitdevnet@gmail.com or zeroitdev@outlook.com
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace Zeroit.Framework.ProgressThematic.FormEditors
 {
     partial class ProgressDialog
     {
